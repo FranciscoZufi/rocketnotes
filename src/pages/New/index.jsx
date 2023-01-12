ec524f654cc192a36b479e4e8667c05ae3a0d347
Francisco <francar76@hotmail.com>
2023-01-12 19:29:07 -0300

    finish new

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,9 @@
 import { Header } from '../../components/header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
 
 import { Container, Form } from './styles'
 
@@ -17,6 +20,18 @@ export function New(){
           </header>
           <Input placeholder="Title"/>
           <Textarea placeholder="comments"/>
+          <Section title="Useful links">
+            <NoteItem value="https://rocketseat.com.br"/>
+            <NoteItem isNew placeholder="New link"/>
+          </Section>
+
+          <Section title="markers">
+            <div className='tags'>
+            <NoteItem value="React"/>
+            <NoteItem isNew placeholder="New tag"/>
+            </div>
+          </Section>
+          <Button title="Save" />
         </Form>
       </main>
     </Container>
