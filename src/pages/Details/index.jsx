4cb3277374ef59e488426400423844dfc07e199e
Francisco <francar76@hotmail.com>
2023-01-12 11:14:42 -0300

    links

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,22 @@
-import {Container} from './styles'
+import {Container, Links} from './styles'
+import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
+import { Section } from '../../components/Section'
 
 export function Details() {
 
 
   return (
     <Container>
-    <h1>Hello Word!</h1>
-    <span>Francisco Zufi</span>,
-
-    <Button/>
+      <Header/>
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+          <li><a href="#">https://www.rocketseat.com.br</a></li>
+        
+        </Links>
+      </Section>
+       <Button title="Voltar" />
     </Container>
     
   )
