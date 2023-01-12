8302c4fdc42fbd7f0442487795214032310b7969
Francisco <francar76@hotmail.com>
2023-01-12 13:27:05 -0300

    details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,7 +1,9 @@
-import {Container, Links} from './styles'
+import {Container, Links, Content} from './styles'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
 import { Section } from '../../components/Section'
+import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
 
@@ -9,6 +11,12 @@ export function Details() {
   return (
     <Container>
       <Header/>
+      <main>
+        <Content>
+      <ButtonText title='Excluir nota'/>
+      
+      <h1>Introdução ao React</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam beatae omnis ullam consequatur, consectetur ut facere tenetur maiores. Hic id error quidem fuga blanditiis molestias pariatur explicabo molestiae et illo?</p>
       <Section title="Links úteis">
         <Links>
           <li><a href="#">https://www.rocketseat.com.br</a></li>
@@ -16,7 +24,14 @@ export function Details() {
         
         </Links>
       </Section>
+      <Section title="Marcadores">
+        <Tag title='express' />
+        <Tag title='nodejs' />
+    
+      </Section>
        <Button title="Voltar" />
+       </Content>
+      </main>
     </Container>
     
   )
