75f09691c2fc66110452371f0fe70d07cb71b885
Francisco <francar76@hotmail.com>
2023-01-16 13:50:49 -0300

    auth

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import { Routes } from './routes'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './hooks/auth';
+
 import theme from './styles/theme'
 
 
@@ -11,7 +13,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
   
--- src/pages/Singin/index.jsx
+++ src/pages/Singin/index.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react'
 import { Container, Form, Background } from './styles'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 import { Link } from 'react-router-dom'
 import { FiMail, FiLock} from 'react-icons/fi'
 
+import { useAuth} from '../../hooks/auth'
+
 export function SingIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { singIn } = useAuth()
+
+  function handleSignIn(){
+    singIn({email, password})
+  }
 return (
   <Container>
     <Form>
@@ -13,9 +23,9 @@ return (
 
       <h2>LogIn</h2>
 
-      <Input placeholder="E-mail" type="text" icon={FiMail}/>
-      <Input placeholder="Password" type="password" icon={FiLock}/>
-      <Button title="Enter"/>
+      <Input placeholder="E-mail" type="text" icon={FiMail} onChange={e => setEmail(e.target.value)}/>
+      <Input placeholder="Password" type="password" icon={FiLock} onChange={e => setPassword(e.target.value)}/>
+      <Button title="Enter" onClick={handleSignIn}/>
       <Link to='/register'>Create account</Link>
     </Form>
     <Background/>
